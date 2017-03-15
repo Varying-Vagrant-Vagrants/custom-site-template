0cfafedba8b4e5a5b10adb35c907974a84ab1409
LoreleiAurora <iam@l5i.me>
2017-03-15 02:12:38 +0000

    Strip dashed in DB_NAME

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -7,7 +7,7 @@ SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
 WP_VERSION=`get_config_value 'wp_version' 'latest'`
 WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
-DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*]/}
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
