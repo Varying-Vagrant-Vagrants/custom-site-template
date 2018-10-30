0fbbd3d2b821ea8e2b3ee690d5b8e9ce2263e036
Tom J Nowell <contact@tomjn.com>
2018-10-30 17:12:49 +0000

    Prefix the nginx logs

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -17,8 +17,8 @@ echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
 mkdir -p ${VVV_PATH_TO_SITE}/log
-touch ${VVV_PATH_TO_SITE}/log/error.log
-touch ${VVV_PATH_TO_SITE}/log/access.log
+touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
+touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
 
 # Install and configure the latest stable version of WordPress
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
