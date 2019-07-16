4f555ef220d34e89245afe81b8472901f64bc45b
Tom J Nowell <contact@tomjn.com>
2019-07-16 16:32:00 +0100

    renamed to wpconfig_constants

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -83,7 +83,7 @@ else
     sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 fi
 
-cat test.yaml | shyaml get-values-0 sites.${SITE_ESCAPED}.custom.constants |
+cat test.yaml | shyaml get-values-0 sites.${SITE_ESCAPED}.custom.wpconfig_constants |
   while IFS='' read -r -d '' key &&
         IFS='' read -r -d '' value; do
       noroot wp config set "${key}" "${value}" --raw
