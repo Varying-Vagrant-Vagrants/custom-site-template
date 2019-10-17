3018e405cd97234d8e9a38a7919cef840594a881
mmcachran <mattmcachran@gmail.com>
2019-10-17 16:57:21 -0400

    Fix typo.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -157,7 +157,7 @@ if [[ -f "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" ]]; then
   CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php"`;
 
   if [[ "${CURRENT_VERSION}" != "${WP_VERSION}" ]]; then
-    echo "Istalling a new version of WordPress..."
+    echo "Installing a new version of WordPress..."
     noroot wp core download --locale="${WP_LOCALE}" --version="${WP_VERSION}" --force
   fi
 fi
