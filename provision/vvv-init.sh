8296f3758107ec475662cfce9c81f4e0a0b9e90d
mmcachran <mattmcachran@gmail.com>
2019-10-18 08:42:03 -0400

    Ensure version is not empty.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -153,12 +153,14 @@ if [ ! -z "${WP_PLUGINS}" ]; then
     done
 fi
 
-if [[ -f "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" ]]; then
-  CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php"`;
+if [[ ! -z "${WP_VERSION}" ]]; then
+  if [[ -f "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" ]]; then
+    CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php"`;
 
-  if [[ "${CURRENT_VERSION}" != "${WP_VERSION}" ]]; then
-    echo "Installing a new version of WordPress..."
-    noroot wp core download --locale="${WP_LOCALE}" --version="${WP_VERSION}" --force
+    if [[ "${CURRENT_VERSION}" != "${WP_VERSION}" ]]; then
+      echo "Installing a new version of WordPress..."
+      noroot wp core download --locale="${WP_LOCALE}" --version="${WP_VERSION}" --force
+    fi
   fi
 fi
 
