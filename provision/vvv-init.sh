13f58183e96e2f483b71b543a4d8006693b1df22
mmcachran <mattmcachran@gmail.com>
2019-10-18 09:59:48 -0400

    :facepalm: Fix comparison.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -158,7 +158,7 @@ if [[ ! -z "${FORCE_WP_VERSION}" ]]; then
   if [[ -f "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" ]]; then
     CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php"`;
 
-    if [[ "${CURRENT_VERSION}" + 0 != "${WP_VERSION}" ]]; then
+    if [[ "${CURRENT_VERSION}" -ne "${WP_VERSION}" ]]; then
       echo "Installing a new version of WordPress..."
       noroot wp core update --locale="${WP_LOCALE}" --version="${WP_VERSION}" --force
     fi
