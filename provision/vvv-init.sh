6b669ac2155fa5c5bce90d724e96de5940a00a0e
mmcachran <mattmcachran@gmail.com>
2019-10-18 10:27:31 -0400

    Only grab the float.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -156,7 +156,7 @@ fi
 
 if [[ ! -z "${FORCE_CHANGE_WP_VERSION}" ]]; then
   if [[ -f "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" ]]; then
-    CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" | awk -F "'" '{print $2}'`
+    CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" | awk -F "'" '{print $2}' | grep -E "\d+"`
 
     if [[ "${CURRENT_VERSION}" != "${WP_VERSION}" ]]; then
       echo "Installing a new version of WordPress..."
