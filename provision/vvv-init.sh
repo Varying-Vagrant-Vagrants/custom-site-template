c598adb912a15f0f3152c3041390554880368e6a
mmcachran <mattmcachran@gmail.com>
2019-10-18 10:59:14 -0400

    Updates version command to remove whitespace with awk instead of grep.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -156,7 +156,7 @@ fi
 
 if [[ ! -z "${FORCE_CHANGE_WP_VERSION}" ]]; then
   if [[ -f "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" ]]; then
-    CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" | awk -F "'" '{print $2}' | grep -E "\d+"`
+    CURRENT_VERSION=`grep wp_version "${VVV_PATH_TO_SITE}/public_html/wp-includes/version.php" | awk -F "'" '{print $2}' | awk "NF > 0"`
 
     if [[ "${CURRENT_VERSION}" != "${WP_VERSION}" ]]; then
       echo "Installing a new version of WordPress..."
