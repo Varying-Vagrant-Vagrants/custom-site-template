aa1680b3cb93b5f38055e56118f697a19128b78b
Daniele Scasciafratte <mte90net@gmail.com>
2019-12-16 16:57:06 +0100

    Adding more verbosity (#34)
    * Adding more verbosity and fixed some typos

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -3,7 +3,7 @@
 
 set -eo pipefail
 
-echo " * Custom site template provisioner - downloads and installs a copy of WP stable for testing, building client sites, etc"
+echo " * Custom site template provisioner ${VVV_SITE_NAME} - downloads and installs a copy of WP stable for testing, building client sites, etc"
 
 # fetch the first host as the primary domain. If none is available, generate a default using the site name
 DOMAIN=$(get_primary_host "${VVV_SITE_NAME}".test)
@@ -32,16 +32,18 @@ noroot mkdir -p "${VVV_PATH_TO_SITE}/public_html"
 
 cd "${VVV_PATH_TO_SITE}/public_html"
 
+echo " * Install type is '${WP_TYPE}'"
+
 if [ "${WP_TYPE}" != "none" ]; then
 
   # Install and configure the latest stable version of WordPress
   if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
-    echo " * Downloading WordPress ${WP_VERSION} ${WP_LOCALE}"
+    echo " * Downloading WordPress version '${WP_VERSION}' locale: '${WP_LOCALE}'"
     noroot wp core download --locale="${WP_LOCALE}" --version="${WP_VERSION}"
   fi
 
   if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
-    echo " * Configuring WordPress"
+    echo " * Setting up wp-config.php"
     noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp  --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'SCRIPT_DEBUG', true );
@@ -51,7 +53,7 @@ PHP
   if ! $(noroot wp core is-installed ); then
     echo " * WordPress is present but isn't installed to the database, checking for SQL dumps in wp-content/database.sql or the main backup folder."
     if [ -f "${VVV_PATH_TO_SITE}/public_html/wp-content/database.sql" ]; then
-      echo " * Found database backup on site directory. Installing site from there..."
+      echo " * Found a database backup at public_html/wp-content/database.sql. Restoring the site"
       noroot wp config set DB_USER "wp"
       noroot wp config set DB_PASSWORD "wp"
       noroot wp config set DB_HOST "localhost"
@@ -59,7 +61,7 @@ PHP
       noroot wp db import "${VVV_PATH_TO_SITE}/public_html/wp-content/database.sql"
       echo " * Installed database backup"
     elif [ -f "/srv/database/backups/${VVV_SITE_NAME}.sql" ]; then
-      echo " * Found database backup in the backups directory. Installing site from there..."
+      echo " * Found database backup at /srv/database/backups/${VVV_SITE_NAME}.sql, restoring the site"
       noroot wp config set DB_USER "wp"
       noroot wp config set DB_PASSWORD "wp"
       noroot wp config set DB_HOST "localhost"
@@ -67,7 +69,7 @@ PHP
       noroot wp db import "/srv/database/backups/${VVV_SITE_NAME}.sql"
       echo " * Installed database backup"
     else
-      echo " * Installing WordPress Stable..."
+      echo " * Installing WordPress Stable"
 
       ADMIN_USER=$(get_config_value 'admin_user' "admin")
       ADMIN_PASSWORD=$(get_config_value 'admin_password' "password")
@@ -90,27 +92,32 @@ PHP
 
       DELETE_DEFAULT_PLUGINS=$(get_config_value 'delete_default_plugins' '')
       if [ ! -z "${DELETE_DEFAULT_PLUGINS}" ]; then
-          noroot wp plugin delete akismet
-          noroot wp plugin delete hello
+        echo " * Deleting the default plugins akismet and hello dolly"
+        noroot wp plugin delete akismet
+        noroot wp plugin delete hello
       fi
 
       INSTALL_TEST_CONTENT=$(get_config_value 'install_test_content' "")
       if [ ! -z "${INSTALL_TEST_CONTENT}" ]; then
-        echo " * Installing test content..."
+        echo " * Downloading test content from github.com/poststatus/wptest/master/wptest.xml"
         curl -s https://raw.githubusercontent.com/poststatus/wptest/master/wptest.xml > import.xml
+        echo " * Installing the wordpress-importer"
         noroot wp plugin install wordpress-importer
+        echo " * Activating the wordpress-importer"
         noroot wp plugin activate wordpress-importer
+        echo " * Importing test data"
         noroot wp import import.xml --authors=create
+        echo " * Cleaning up import.xml"
         rm import.xml
         echo " * Test content installed"
       fi
     fi
   else
     if [[ $(noroot wp core version) > "${WP_VERSION}" ]]; then
-      echo "Installing an older version of WordPress..."
+      echo " * Installing an older version '${WP_VERSION}' of WordPress"
       noroot wp core update --version="${WP_VERSION}" --force
     else
-      echo "Updating WordPress Stable..."
+      echo " * Updating WordPress '${WP_VERSION}'"
       noroot wp core update --version="${WP_VERSION}"
     fi
   fi
@@ -118,17 +125,18 @@ else
   echo " * wp_type was set to none, provisioning WP was skipped, moving to Nginx configs"
 fi
 
-echo "Copying the sites Nginx config template"
+echo " * Copying the sites Nginx config template"
 if [ -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" ]; then
-  echo "A vvv-nginx-custom.conf file was found"
+  echo " * A vvv-nginx-custom.conf file was found"
   cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 else
-  echo "Using the default vvv-nginx-default.conf, to customize, create a vvv-nginx-custom.conf"
+  echo " * Using the default vvv-nginx-default.conf, to customize, create a vvv-nginx-custom.conf"
   cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-default.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 fi
 
 LIVE_URL=$(get_config_value 'live_url' '')
 if [ ! -z "$LIVE_URL" ]; then
+  echo " * Adding support for Live URL redirects to NGINX of the website's media"
   # replace potential protocols, and remove trailing slashes
   LIVE_URL=$(echo "${LIVE_URL}" | sed 's|https://||' | sed 's|http://||'  | sed 's:/*$::')
 
@@ -154,12 +162,14 @@ fi
 get_config_value 'wpconfig_constants' |
   while IFS='' read -r -d '' key &&
         IFS='' read -r -d '' value; do
+      echo " * Adding constant '${key}' with value '${value}' to wp-config.php"
       noroot wp config set "${key}" "${value}" --raw
   done
 
 WP_PLUGINS=$(get_config_value 'install_plugins' '')
 if [ ! -z "${WP_PLUGINS}" ]; then
   for plugin in ${WP_PLUGINS//- /$'\n'}; do
+      echo " * Installing/activating plugin: '${plugin}'"
       noroot wp plugin install "${plugin}" --activate
   done
 fi
@@ -167,8 +177,9 @@ fi
 WP_THEMES=$(get_config_value 'install_themes' '')
 if [ ! -z "${WP_THEMES}" ]; then
     for theme in ${WP_THEMES//- /$'\n'}; do
-        noroot wp theme install "${theme}"
+      echo " * Installing theme: '${theme}'"
+      noroot wp theme install "${theme}"
     done
 fi
 
-echo " * Site Template provisioner script completed"
+echo " * Site Template provisioner script completed for ${VVV_SITE_NAME}"
