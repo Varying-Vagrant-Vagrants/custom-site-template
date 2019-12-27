5f5e3012d2021e78397f3ab62318e7e6a20ed7f9
Daniele Scasciafratte <mte90net@gmail.com>
2019-12-27 17:21:19 +0100

    fix linting

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -159,7 +159,7 @@ else
   sed -i "s#{{LIVE_URL}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 fi
 
-cat ${VVV_CONFIG} | shyaml get-values-0 "sites.${VVV_SITE_NAME}.custom.wpconfig_constants" |
+shyaml get-values-0 "sites.${VVV_SITE_NAME}.custom.wpconfig_constants" < "${VVV_CONFIG}" |
   while IFS='' read -r -d '' key &&
         IFS='' read -r -d '' value; do
       temp_value=$(echo "${value}" | awk '{print tolower($0)}')
