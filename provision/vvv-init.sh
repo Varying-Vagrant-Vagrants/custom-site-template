eb6691758d440b929a1e5fa56604c5ad2c2078d8
Daniele Scasciafratte <mte90net@gmail.com>
2019-12-27 16:37:49 +0100

    Fix wpconfig constants adding

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -159,11 +159,16 @@ else
   sed -i "s#{{LIVE_URL}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 fi
 
-get_config_value 'wpconfig_constants' |
+$(shyaml get-values "sites.${VVV_SITE_NAME}.custom.wpconfig_constants" 2> /dev/null < ${VVV_CONFIG}) |
   while IFS='' read -r -d '' key &&
         IFS='' read -r -d '' value; do
+      $temp_value = $(echo "${value}" | awk '{print tolower($0)}')
       echo " * Adding constant '${key}' with value '${value}' to wp-config.php"
-      noroot wp config set "${key}" "${value}" --raw
+      if [ "${temp_value}" -eq "true" ] || [ "${temp_value}" -eq "false" ]; then
+        noroot wp config set "${key}" "${value}" --raw
+      else
+        noroot wp config set "${key}" "${value}"
+      fi
   done
 
 WP_PLUGINS=$(get_config_value 'install_plugins' '')
